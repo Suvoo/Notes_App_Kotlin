211f24827013fcffbe2a362902faa467b1f624b1
casa98 <cagudeloa@uninorte.edu.co>
2020-10-27 14:33:31 -0500

    Update model (Note.kt) to use Kotlin Data Class. Fix title when adding note

--- app/src/main/java/com/suvodeep/notesapp/AddNotes.kt
+++ app/src/main/java/com/suvodeep/notesapp/AddNotes.kt
@@ -8,8 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_add_notes.*
 
 class AddNotes : AppCompatActivity() {
-    val dbTable="Notes"
-    var id=0
+    val dbTable = "Notes"
+    var id = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_notes)
@@ -17,28 +17,29 @@ class AddNotes : AppCompatActivity() {
 
 
 
-        try{
-            var bundle:Bundle= intent.extras!!
-            id=bundle.getInt("ID",0)
-            if(id!=0) {
-                etTitle.text = bundle.getString("name")
-                etDes.setText(bundle.getString("des") )
+        try {
+            var bundle: Bundle = intent.extras!!
+            id = bundle.getInt("ID", 0)
+            if (id != 0) {
+                etTitle.setText(bundle.getString("name"))
+                etDes.setText(bundle.getString("des"))
 
             }
-        }catch (ex:Exception){}
+        } catch (ex: Exception) {
+        }
 
 
     }
 
-    fun  buAdd(view: View){
-        var dbManager=DbManager(this)
+    fun buAdd(view: View) {
+        var dbManager = DbManager(this)
 
-        var values= ContentValues()
-        values.put("Title",etTitle.text.toString())
-        values.put("Description",etDes.text.toString())
+        var values = ContentValues()
+        values.put("Title", etTitle.text.toString())
+        values.put("Description", etDes.text.toString())
 
 
-        if(id==0) {
+        if (id == 0) {
             val ID = dbManager.Insert(values)
             if (ID > 0) {
                 Toast.makeText(this, " note is added", Toast.LENGTH_LONG).show()
@@ -46,9 +47,9 @@ class AddNotes : AppCompatActivity() {
             } else {
                 Toast.makeText(this, " cannot add note ", Toast.LENGTH_LONG).show()
             }
-        }else{
-            var selectionArs= arrayOf(id.toString())
-            val ID = dbManager.Update(values,"ID=?",selectionArs)
+        } else {
+            var selectionArs = arrayOf(id.toString())
+            val ID = dbManager.Update(values, "ID=?", selectionArs)
             if (ID > 0) {
                 Toast.makeText(this, " note is added", Toast.LENGTH_LONG).show()
                 finish()
